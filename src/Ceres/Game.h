0e09770db43d22fdaa11c7438107ab8683c1f760
Redesigner <stephenwmelnick@gmail.com>
2021-05-23 15:09:36 -0400

    added game class

---
+++ src/Ceres/Game.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Game
+{
+public:
+    
+    Game(/* args */);
+    
+    ~Game();
+
+private:
+    /* data */
+
+};
--- src/Ceres/main.cpp
+++ src/Ceres/main.cpp
@@ -1,4 +1,5 @@
-extern "C"
+#include "Game.h"
+extern "C" 
 {
 	#include <SDL2/SDL.h>
 }
@@ -8,8 +9,8 @@ const int SCREEN_HEIGHT = 720;
 
 int main(int, char**)
 {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
+	SDL_Window* window = nullptr;
+	SDL_Surface* screenSurface = nullptr;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
@@ -17,14 +18,15 @@ int main(int, char**)
 	else
 	{
 		window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window != NULL)
+		if (window != nullptr)
 		{
 			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 			SDL_UpdateWindowSurface(window);
 			SDL_Delay(1000);
 		}
 	}
+	Game game;
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
